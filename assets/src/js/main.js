deecfa1fe325cc0f768ce169c2c89177c2805a3c
RightTurnClyde <RightTurnclyde@users.noreply.github.com>
2014-09-05 09:07:59 -0400

    changed main.js

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,10 +1,25 @@
-var Errors = (function() {
+$(function() {       
+    "use strict";
+    $.ajaxSetup({
+        cache:false,
+        error:function(x,e){
+            var sessionTimeout = x.getResponseHeader("sessionTimeout");
+            if (sessionTimeout) {
+                alert("Your session has expired");
+                location.href = "index.cfm?action=home:security.login"; 
+            } else {
+                alert("An error occurred accessing remote method."); 
+            }               
+        }
+    }); 
+});   
 
+var Errors = (function() {
+    "use strict";
     var showErrors = function(errors) {
         var str = "Please review the following errors:\n";
         
         $.each(errors,function(i, err) {
-            //str += "<li>" + err + "</li>";
             str += "\n- " + err;
         });
         
